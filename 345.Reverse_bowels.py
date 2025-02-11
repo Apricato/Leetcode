d8773912bd07012ac36da4128537bcdaab4933aa
Apricato <quelqueaout@gmail.com>
2025-02-11 08:01:23 -0600

    354 reverse string

---
+++ 345.Reverse_bowels.py
@@ -0,0 +1,11 @@
+'''
+345.REVERSE BOWELS OF A STRING
+
+Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both 
+lower and upper cases, more than once. 
+
+ '''
+
+
+import 
